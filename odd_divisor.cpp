c3e832e07faa445c0f59b9c29bf24157602e14a3
parag140 <guptaparag1401@gmail.com>
2025-08-19 02:54:55 +0530

    auto commit

--- odd_divisor.cpp
+++ odd_divisor.cpp
@@ -6,17 +6,6 @@ int main(){
     while(t--){
         long long n;
         cin>>n;
-        if(n%2!=0){
-            cout<<"YES"<<endl;
-            continue;
-        }
-        while(n!=1){
-            if(n%2!=0){
-                cout<<"YES"<<endl;
-                break;
-            }
-            n/=2;
-        }
-        cout<<"NO"<<endl;
+        
     }
 }
