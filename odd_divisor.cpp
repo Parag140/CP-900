b92b00dee1aee651652fd116c63509e8288260ae
parag140 <guptaparag1401@gmail.com>
2025-08-19 02:50:54 +0530

    auto commit

--- odd_divisor.cpp
+++ odd_divisor.cpp
@@ -15,7 +15,7 @@ int main(){
                 cout<<"YES"<<endl;
                 break;
             }
-           if(n%2 == 0 || n%4)
+           if(n%2 == 0)
         }
         cout<<"NO"<<endl;
     }
