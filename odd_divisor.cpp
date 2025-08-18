ae7b957c28177105f845aec48ae99d2e21e2478c
parag140 <guptaparag1401@gmail.com>
2025-08-19 02:59:12 +0530

    auto commit

--- odd_divisor.cpp
+++ odd_divisor.cpp
@@ -8,6 +8,6 @@ int main(){
         cin>>n;
         //we have to check wheter the last bit is on or off
         // agar kuch 2 ki power me nahi hoga to uska odd divisor exist karega hi
-        
+        for(int i = )
     }
 }
